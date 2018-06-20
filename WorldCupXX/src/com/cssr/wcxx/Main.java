da80974ad350613fcd863f7020127539310b56b3
Ray3 <ray3@github.com>
2018-06-20 14:52:32 -0500

    Codebase After Clone  demo By ray

--- WorldCupXX/src/com/cssr/wcxx/Main.java
+++ WorldCupXX/src/com/cssr/wcxx/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Master CodeBase");
-        System.out.println("yuh - Lee-Roy");
-        System.out.println("CodeBase After Clon");
+        System.out.println("yuh - Lee-Ro");
+        System.out.println("CodeBase After Clone");
     }
 }
