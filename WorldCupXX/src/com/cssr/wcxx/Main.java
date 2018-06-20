3e3a6e7004599c05177e0e12e6a9441996cbbf3c
Ray3 <ray3@github.com>
2018-06-20 13:29:01 -0500

    Master CodeBase By Ray

---
+++ WorldCupXX/src/com/cssr/wcxx/Main.java
@@ -0,0 +1,9 @@
+package com.cssr.wcxx;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Master CodeBase");
+    }
+}
